45e1a42ad55f9e036f7bddf38c9668b8a98289fc
Denis <deniskhuharev@gmail.com>
2025-11-12 20:58:35 +0700

    Fix test with get question and answers

--- qa_autotest.py
+++ qa_autotest.py
@@ -4,6 +4,7 @@ import json
 
 BASE_URL = "http://127.0.0.1:8000"
 
+
 # Тестирование API с добавлением данных и последующим их удалением
 
 class TestAPI(unittest.TestCase):
@@ -27,133 +28,154 @@ class TestAPI(unittest.TestCase):
         data = {
             "text": "Test message"
         }
-        response = requests.post(f"{BASE_URL}/questions/", json = data)
+        response = requests.post(f"{BASE_URL}/questions/", json=data)
         self.assertStatus(response, 200)
         q_id = response.json()["question"]["id"]
         requests.delete(f"{BASE_URL}/questions/{q_id}")
 
     def test_get_question(self):
         """Получение вопроса и ответов на него"""
-        question_id = 29
-        response = requests.get(f"{BASE_URL}/questions/{question_id}")
-        self.assertStatus(response, 200)
-
-    def test_get_wrong_question(self):
-        """Получение вопроса, которого не существует"""
-        question_id = 999
-        response = requests.get(f"{BASE_URL}/questions/{question_id}")
-        self.assertStatus(response, 404)
-
-    def test_delete_question(self):
-        """Удаление вопроса"""
-        data = {
-            "text": "Test message"
-        }
-        create_response = requests.post(f"{BASE_URL}/questions/", json = data)
-        self.assertStatus(create_response, 200)
-        id = create_response.json()["question"]["id"]
-        response = requests.delete(f"{BASE_URL}/questions/{id}")
-        self.assertStatus(response, 200)
-
-    def test_delete_wrong_question(self):
-        """Удаление вопроса"""
-        id = 999
-        response = requests.delete(f"{BASE_URL}/questions/{id}")
-        self.assertStatus(response, 404)
-
-    def test_get_answer(self):
-        """Получение корректного ответа"""
         question_data = {
             "text": "Test message"
         }
-        create_question = requests.post(f"{BASE_URL}/questions/", json = question_data)
+
+        create_question = requests.post(f"{BASE_URL}/questions/", json=question_data)
         self.assertStatus(create_question, 200)
         q_id = create_question.json()["question"]["id"]
         answer_data = {
             "text": "Test Message",
             "user_id": "testid"
         }
-        create_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json = answer_data)
+        create_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json=answer_data)
         self.assertStatus(create_answer, 200)
-        a_id = create_answer.json()["answer"]["id"]
-        response = requests.get(f"{BASE_URL}/answers/{a_id}")
+        response = requests.get(f"{BASE_URL}/questions/{q_id}")
         self.assertStatus(response, 200)
 
-        requests.delete(f"{BASE_URL}/answers/{a_id}")
-        requests.delete(f"{BASE_URL}/questions/{q_id}")
-
-    def test_get_wrong_answer(self):
-        """Получение ответа с неверным ID"""
-        a_id = 999
-        response = requests.get(f"{BASE_URL}/answers/{a_id}")
-        self.assertStatus(response, 404)
-
-    def test_delete_answer(self):
-        """Удаление ответа"""
-        question_data = {
-            "text": "Test message"
-        }
-        create_question = requests.post(f"{BASE_URL}/questions/", json = question_data)
-        self.assertStatus(create_question, 200)
-        q_id = create_question.json()["question"]["id"]
-        answer_data = {
-            "text": "Test Message",
-            "user_id": "testid"
-        }
-        create_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json = answer_data)
-        self.assertStatus(create_answer, 200)
-        a_id = create_answer.json()["answer"]["id"]
-        response = requests.delete(f"{BASE_URL}/answers/{a_id}")
-        self.assertStatus(response, 200)
-        requests.delete(f"{BASE_URL}/answers/{a_id}")
-        requests.delete(f"{BASE_URL}/questions/{q_id}")
-
-    def test_delete_wrong_answer(self):
-        """Удаление несуществующего ответа"""
-        a_id = 999
-        response = requests.delete(f"{BASE_URL}/answers/{a_id}")
-        self.assertStatus(response, 404)
-
-    def test_create_answer(self):
-        """Добавление ответа"""
-        question_data = {
-            "text": "Test message"
-        }
-        create_question = requests.post(f"{BASE_URL}/questions/", json = question_data)
-        self.assertStatus(create_question, 200)
-        q_id = create_question.json()["question"]["id"]
-        answer_data = {
-            "text": "Test Message",
-            "user_id": "testid"
-        }
-        create_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json = answer_data)
-        self.assertStatus(create_answer, 200)
-
-        a_id = create_answer.json()["answer"]["id"]
-        requests.delete(f"{BASE_URL}/answers/{a_id}")
-        requests.delete(f"{BASE_URL}/questions/{q_id}")
-
-    def test_create_second_answer_by_user(self):
-        """Добавление ответа"""
-        question_data = {
-            "text": "Test message"
-        }
-        create_question = requests.post(f"{BASE_URL}/questions/", json = question_data)
-        self.assertStatus(create_question, 200)
-        q_id = create_question.json()["question"]["id"]
-        answer_data = {
-            "text": "Test Message",
-            "user_id": "testid"
-        }
-        create_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json = answer_data)
-        self.assertStatus(create_answer, 200)
-        a_id = create_answer.json()["answer"]["id"]
-        create_second_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json = answer_data)
-        self.assertStatus(create_second_answer, 400)
 
 
-        requests.delete(f"{BASE_URL}/answers/{a_id}")
-        requests.delete(f"{BASE_URL}/questions/{q_id}")
+def test_get_wrong_question(self):
+    """Получение вопроса, которого не существует"""
+    question_id = 999
+    response = requests.get(f"{BASE_URL}/questions/{question_id}")
+    self.assertStatus(response, 404)
+
+
+def test_delete_question(self):
+    """Удаление вопроса"""
+    data = {
+        "text": "Test message"
+    }
+    create_response = requests.post(f"{BASE_URL}/questions/", json=data)
+    self.assertStatus(create_response, 200)
+    id = create_response.json()["question"]["id"]
+    response = requests.delete(f"{BASE_URL}/questions/{id}")
+    self.assertStatus(response, 200)
+
+
+def test_delete_wrong_question(self):
+    """Удаление вопроса"""
+    id = 999
+    response = requests.delete(f"{BASE_URL}/questions/{id}")
+    self.assertStatus(response, 404)
+
+
+def test_get_answer(self):
+    """Получение корректного ответа"""
+    question_data = {
+        "text": "Test message"
+    }
+    create_question = requests.post(f"{BASE_URL}/questions/", json=question_data)
+    self.assertStatus(create_question, 200)
+    q_id = create_question.json()["question"]["id"]
+    answer_data = {
+        "text": "Test Message",
+        "user_id": "testid"
+    }
+    create_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json=answer_data)
+    self.assertStatus(create_answer, 200)
+    a_id = create_answer.json()["answer"]["id"]
+    response = requests.get(f"{BASE_URL}/answers/{a_id}")
+    self.assertStatus(response, 200)
+
+    requests.delete(f"{BASE_URL}/answers/{a_id}")
+    requests.delete(f"{BASE_URL}/questions/{q_id}")
+
+
+def test_get_wrong_answer(self):
+    """Получение ответа с неверным ID"""
+    a_id = 999
+    response = requests.get(f"{BASE_URL}/answers/{a_id}")
+    self.assertStatus(response, 404)
+
+
+def test_delete_answer(self):
+    """Удаление ответа"""
+    question_data = {
+        "text": "Test message"
+    }
+    create_question = requests.post(f"{BASE_URL}/questions/", json=question_data)
+    self.assertStatus(create_question, 200)
+    q_id = create_question.json()["question"]["id"]
+    answer_data = {
+        "text": "Test Message",
+        "user_id": "testid"
+    }
+    create_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json=answer_data)
+    self.assertStatus(create_answer, 200)
+    a_id = create_answer.json()["answer"]["id"]
+    response = requests.delete(f"{BASE_URL}/answers/{a_id}")
+    self.assertStatus(response, 200)
+    requests.delete(f"{BASE_URL}/answers/{a_id}")
+    requests.delete(f"{BASE_URL}/questions/{q_id}")
+
+
+def test_delete_wrong_answer(self):
+    """Удаление несуществующего ответа"""
+    a_id = 999
+    response = requests.delete(f"{BASE_URL}/answers/{a_id}")
+    self.assertStatus(response, 404)
+
+
+def test_create_answer(self):
+    """Добавление ответа"""
+    question_data = {
+        "text": "Test message"
+    }
+    create_question = requests.post(f"{BASE_URL}/questions/", json=question_data)
+    self.assertStatus(create_question, 200)
+    q_id = create_question.json()["question"]["id"]
+    answer_data = {
+        "text": "Test Message",
+        "user_id": "testid"
+    }
+    create_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json=answer_data)
+    self.assertStatus(create_answer, 200)
+
+    a_id = create_answer.json()["answer"]["id"]
+    requests.delete(f"{BASE_URL}/answers/{a_id}")
+    requests.delete(f"{BASE_URL}/questions/{q_id}")
+
+
+def test_create_second_answer_by_user(self):
+    """Добавление ответа"""
+    question_data = {
+        "text": "Test message"
+    }
+    create_question = requests.post(f"{BASE_URL}/questions/", json=question_data)
+    self.assertStatus(create_question, 200)
+    q_id = create_question.json()["question"]["id"]
+    answer_data = {
+        "text": "Test Message",
+        "user_id": "testid"
+    }
+    create_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json=answer_data)
+    self.assertStatus(create_answer, 200)
+    a_id = create_answer.json()["answer"]["id"]
+    create_second_answer = requests.post(f"{BASE_URL}/questions/{q_id}/answers/", json=answer_data)
+    self.assertStatus(create_second_answer, 400)
+
+    requests.delete(f"{BASE_URL}/answers/{a_id}")
+    requests.delete(f"{BASE_URL}/questions/{q_id}")
 
 
 if __name__ == "__main__":
